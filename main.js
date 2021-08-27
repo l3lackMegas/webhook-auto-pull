9070db00a43aa94c352249d916d1fc05c30f3e83
Jaruwat Pohong <jara7897@gmail.com>
2021-08-28 01:36:17 +0700

    feat: implement diagnosis on error message

--- main.js
+++ main.js
@@ -18,22 +18,39 @@ app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb' }));
  
 app.post('/', async function (req, res) {
-    if(req.query.repo && req.query.key == CONFIGS.key) { // Check key from configs.json
+    let item_namespace = req.query.repo || req.query.item
+    if(item_namespace && req.query.key == CONFIGS.key) { // Check key from configs.json
 
-        let repoInfo = CONFIGS.repo[req.query.repo],
+        let repoInfo = CONFIGS.repo[item_namespace],
             payload = JSON.parse(req.body.payload)
 
-        if(!payload.ref) { // Check payload
-            console.log(`Can't find refernce.`)
-            res.json({status: false, message: `Can't find refernce.`})
+        if(!payload.ref && !payload.zen) { // Check payload
+            console.log(`[${item_namespace}] Can't find refernce.`)
+            res.json({
+                status: false,
+                message: `Can't find refernce.`,
+                diagnosis: `Auto pull can't find target repo from webhook's payload. Please check the webhook payload and open issue to https://github.com/l3lackMegas/webhook-auto-pull/issues`
+            })
+            return 0
+        } else if(!payload.ref && payload.zen) { // Check payload
+            console.log(`[${item_namespace}] Init autopull.`)
+            res.json({
+                status: false,
+                message: `[Ignore] This is a request from webhook's test. (${payload.zen})`,
+                diagnosis: `Let do commit to your target branch!`
+            })
             return 0
         }
 
         if(!repoInfo) {
-            console.log(`Can not found this repo. [${req.query.repo}]`)
+            console.log(`[${item_namespace}] Can not found this item from configs. [${item_namespace}]`)
             
             res.status(500) 
-            res.json({status: false, message: `Can not found this repo. [${req.query.repo}]`})
+            res.json({
+                status: false,
+                message: `Can not found this item from configs. [${item_namespace}]`,
+                diagnosis: `Please check config's namespace that look like &repo=item_namespace from your url.`
+            })
             return 0
         }
 
@@ -43,8 +60,8 @@ app.post('/', async function (req, res) {
             const REPO = repoInfo.url;
             const remote = `https://${USER}:${PASS}@${REPO}`; // Remote url
 
-            console.log(`Start pulling [${repoInfo.branch}] with `, remote)
-            console.log("To path: ", repoInfo.path)
+            console.log(`[${item_namespace}] Start pulling (${repoInfo.branch}) with `, remote)
+            console.log(`[${item_namespace}] To path: `, repoInfo.path)
 
             try {
                 await git().init();
@@ -55,61 +72,77 @@ app.post('/', async function (req, res) {
             }
             catch (err) { 
                 /* handle all errors here */
-                console.log("Fail." + err)
+                console.log(`[${item_namespace}] Fail.` + err)
 
                 // Response success
                 res.status(500)
-                res.json({status: false, message: 'Fail: ' + err})
+                res.json({
+                    status: false,
+                    message: 'Fail: ' + err,
+                    diagnosis: `This is error from server. Please contact server owner or open issue to https://github.com/l3lackMegas/webhook-auto-pull/issues`
+                })
                 return 0
             }
             // Response success
             git(repoInfo.path).pull(remote, repoInfo.branch).then((status) => { // Start pulling
                 
-                console.log("Pull Finish.")
+                console.log(`[${item_namespace}] Pull Finish.`)
 
                 if(repoInfo.script) { // Check if script exist. You can custom script by configs.json
 
-                    console.log("Running script: ", repoInfo.script)
+                    console.log(`[${item_namespace}] Running script: `, repoInfo.script)
 
                     exec(repoInfo.script, { // Execute script after pull repo
                         cwd: repoInfo.path
                     }, (error, stdout, stderr) => {
                         if (error) {
-                            console.log(`error: ${error.message}`);
+                            console.log(`[${item_namespace}] error: ${error.message}`);
                             return;
                         }
                         if (stderr) {
-                            console.log(`stderr: ${stderr}`);
+                            console.log(`[${item_namespace}] stderr: ${stderr}`);
                             return;
                         }
 
-                        console.log(`stdout: ${stdout}`);
+                        console.log(`[${item_namespace}] stdout: ${stdout}`);
                     });
                 }
             })
             .catch((err) => ()=>{
-                console.log("Fail." + err)
+                console.log(`[${item_namespace}] Fail.` + err)
 
                 // Response success
                 res.status(500)
-                res.json({status: false, message: 'Fail: ' + err})
+                res.json({
+                    status: false,
+                    message: 'Fail: ' + err,
+                    diagnosis: `This error is from simple-git package. Please check error from official repo. (https://github.com/steveukx/git-js)`
+                })
             })
             
             
             return 0
         } else if(repoInfo && payload.ref.split('/')[2] != repoInfo.branch) { // In case doesn't the target branch for pull
 
-            console.log(`Not target branch [${payload.ref.split('/')[2]}].`)
+            console.log(`[${item_namespace}] Not target branch [${payload.ref.split('/')[2]}].`)
             
-            res.status(500) 
-            res.json({status: false, message: 'Not target branch.'})
+            res.status(202) 
+            res.json({
+                status: false,
+                message: '[Ignore] Not target branch.',
+                diagnosis: `Try to check your branch that you commit is same with configs file.`
+            })
             return 0
         }
     }
 
-    console.log("Wrong request.")
+    console.log(`[${item_namespace}] Wrong request.`)
     res.status(500)
-    res.json({status: false, message: 'Wrong request.'}) // Can't accept this request, because got wrong key
+    res.json({
+        status: false,
+        message: 'Wrong request.',
+        diagnosis: `Can't accept this request, because got wrong key. Please check key from config your config`
+    }) // Can't accept this request, because got wrong key
 })
 
 app.get('*', (req, res)=>res.send(`Hey, Why are you here? 👀`))
